263a73ff3d56f14009e93a8bcbcbb9e301dde17b
an0nymID <m.ihsan2131@gmail.com>
2024-05-13 23:06:55 +0800

    cd action 2

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 /* istanbul ignore file */
-import container from '../src/Infrastructures/container';
-import createServer from '../src/Infrastructures/http/createServer';
+const container = require('../src/Infrastructures/container');
+const createServer = require('../src/Infrastructures/http/createServer');
 
 // INFO : Hack so i can deploy it to vercel, though it will be slower
-export default async function (request, response) {
+module.exports = async function (request, response) {
   const server = await createServer(container);
 
-  const { body, headers, method, url } = request;
+  const {
+    body, headers, method, url,
+  } = request;
 
   const result = await server.inject({
     method,
